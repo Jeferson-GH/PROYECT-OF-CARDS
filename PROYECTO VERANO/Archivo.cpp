6881e373227eb7a0c1301170bf1367eacaab7130
Jeferson-GH <alberto.gh.1824.31@gmail.com>
2024-02-10 15:34:13 -0600

    Practico archivos

--- PROYECTO VERANO/Archivo.cpp
+++ PROYECTO VERANO/Archivo.cpp
@@ -12,7 +12,7 @@ void Archivo::guardarPartida(Lista* lista, Mazo* baraja, Nodo* jugadorActual) //
 	archivo.open(nombreArchivo, std::ios::app); //Se abre el archivo para escritura
 
 	if (!archivo.is_open()) //Termina la funcion si no se abrio el archivo
-		return; 
+		return;
 
 	std::string nicknameJugadorActual = jugadorActual->dato->getNickname(); //Se guarda el nickname del jugador actual
 	jugadorActual = lista->getInicio(); // Se actualiza el jugador actual al inicio de la lista
@@ -23,14 +23,14 @@ void Archivo::guardarPartida(Lista* lista, Mazo* baraja, Nodo* jugadorActual) //
 	while (jugadorActual != nullptr) { // Recorre la lista de jugadores
 		archivo << jugadorActual->dato->getMano()->getCantidad() << '\n'; // Se escribe la cantidad de cartas en la mano del jugador actual
 		archivo << jugadorActual->dato->guardarMano(); // Se guardan las cartas de la mano del jugador actual
-		jugadorActual = jugadorActual->next; 
+		jugadorActual = jugadorActual->next;
 	}
 	archivo << nicknameJugadorActual << '\n'; // Se guarda el nickname del jugador que conserva el turno
 	archivo << baraja->guardarMazo() << '\n'; // Se guardan las cartas restantes en el mazo
 	archivo.close(); // Se cierra el archivo
 }
 
-bool Archivo::cargarPartida(std::string nombreArchivo, Lista* &lista, Mazo* &baraja, Dealer* &dealer, Nodo* &jugadorActual)
+bool Archivo::cargarPartida(std::string nombreArchivo, Lista*& lista, Mazo*& baraja, Dealer*& dealer, Nodo*& jugadorActual)
 {
 	std::ifstream archivo;
 	archivo.open(nombreArchivo); // Se abre el archivo para lectura
@@ -38,49 +38,49 @@ bool Archivo::cargarPartida(std::string nombreArchivo, Lista* &lista, Mazo* &bar
 		return false;
 
 	std::string nombres; //Se declaran distintas variables para diferentes valores de datos
-	char tipo; 
-	int cantidadJugadores, valores, x; 
+	char tipo;
+	int cantidadJugadores, valores, x;
 	archivo >> cantidadJugadores; // Se lee la cantidad de jugadores guardados en el archivo
 	for (int i = 1; i < cantidadJugadores; i++) { // Se itera para leer los nombres de los jugadores
-		archivo >> nombres; 
+		archivo >> nombres;
 		lista->insertar(new Jugador(nombres)); // Se inserta un nuevo jugador en la lista
 	}
 	delete dealer; // Se libera la memoria del dealer anterior para crear uno nuevo
-	dealer = new Dealer; 
+	dealer = new Dealer;
 	lista->insertar(dealer); // Se inserta el dealer en la lista
 	jugadorActual = lista->getInicio(); // Se asigna el jugador actual como el primer jugador de la lista
 	for (int i = 0; i < cantidadJugadores; i++) { //Leen las manos de los jugadores
 		archivo >> x; // Se lee la cantidad de cartas en la mano del jugador actual
 		for (int i = 0; i < x; i++) { // Se itera para leer cada carta en la mano del jugador actual
 			Carta* carta = new Carta;
-			archivo >> valores; 
-			carta->setValor(valores); 
-			archivo >> valores; 
-			carta->setPalo(valores); 
-			archivo >> tipo; 
-			carta->setTipo(tipo); 
-			archivo >> valores; 
-			carta->setBocaAbajo(valores); 
+			archivo >> valores;
+			carta->setValor(valores);
+			archivo >> valores;
+			carta->setPalo(valores);
+			archivo >> tipo;
+			carta->setTipo(tipo);
+			archivo >> valores;
+			carta->setBocaAbajo(valores);
 
 			jugadorActual->dato->getMano()->agregarCarta(carta); // Se agrega la carta a la mano del jugador actual
 		}
-		jugadorActual = jugadorActual->next; 
+		jugadorActual = jugadorActual->next;
 	}
-	archivo >> nombres; 
+	archivo >> nombres;
 	jugadorActual = lista->getJugador(nombres); // Se asigna el jugador actual como el jugador que conservo el turno en la partida guardada
 
 	int cantidad = 0; // Variable para almacenar la cantidad de cartas en el mazo
 	while (archivo >> valores) { // Se itera para leer las cartas restantes en el mazo
-		Carta* carta = new Carta; 
-		carta->setValor(valores); 
-		archivo >> valores; 
+		Carta* carta = new Carta;
+		carta->setValor(valores);
+		archivo >> valores;
 		carta->setPalo(valores);
-		archivo >> tipo; 
-		carta->setTipo(tipo); 
-		archivo >> valores; 
+		archivo >> tipo;
+		carta->setTipo(tipo);
+		archivo >> valores;
 		carta->setBocaAbajo(valores);
 		baraja->cambiarCarta(carta, cantidad); // Se cambia la carta en el mazo
-		cantidad++; 
+		cantidad++;
 		delete carta; //Liberamos memoria de la carta creada
 	}
 	baraja->setCantidad(cantidad); // Se establece la cantidad total de cartas en el mazo
