8f500d650f7d30d8d5ef34aff608356ca7ae528c
azrxl <emiliano.medina.contreras@est.una.ac.cr>
2024-02-11 15:59:55 -0600

    Final

--- PROYECTO VERANO/main.cpp
+++ PROYECTO VERANO/main.cpp
@@ -9,3 +9,12 @@ int main() {
 
 	return 0;
 }
+
+/*
+Estudiantes:
+
+	Jeferson David Garita Hernández | 2-0860-0321
+	Ahymará Ureña Chanto | 3-0536-0049
+	Emiliano Medina Contreras | 1-1949-0422
+
+*/
