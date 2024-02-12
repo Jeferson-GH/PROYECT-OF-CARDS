69b99c70955d8ae77627737944b5a18dd879edfd
Jeferson-GH <alberto.gh.1824.31@gmail.com>
2024-02-11 22:43:48 -0600

    FINAL

--- PROYECTO VERANO/main.cpp
+++ PROYECTO VERANO/main.cpp
@@ -1,20 +1,14 @@
 #include <iostream>
 #include "Juego.h"
-
 int main() {
-	
 	Juego* juego = new Juego;
 	juego->jugar();
 	delete juego;
-
 	return 0;
 }
-
 /*
 Estudiantes:
-
 	Jeferson David Garita Hernandez | 2-0860-0321
 	Ahymara Urena Chanto | 3-0536-0049
 	Emiliano Medina Contreras | 1-1949-0422
-
 */
